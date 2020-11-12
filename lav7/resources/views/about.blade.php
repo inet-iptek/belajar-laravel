937828cbbaeaebc2d5705ad675db0bc33990d5dd
renoagil87 <renoagilsaputra87@gmail.com>
2020-11-12 20:57:11 +0700

    footer

--- lav7/resources/views/about.blade.php
+++ lav7/resources/views/about.blade.php
@@ -1,4 +1,7 @@
 @extends('layout.app')
+@section('title')
+ | {{ $title }}
+@endsection
 @section('content')
 <h1>Hello, My Name is {{ $nama }}</h1>
 @endsection
--- lav7/resources/views/layout/app.blade.php
+++ lav7/resources/views/layout/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Belajar Laravel</title>
+    <title>Belajar Laravel @yield('title')</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
@@ -32,6 +32,9 @@
                 @yield('content')
             </div>
         </div>
+        <footer>
+            <p class="text-center">Copyright by iNET {{ date('Y') }}</p>
+        </footer>
     </div>
 
 
